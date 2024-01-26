4ef2b0d8296795e4452f0c9a441d28a2eeb1f1d9
Ryan Cavalcanti Rocha <ryan@MacBook-Air-de-Ryan.local>
2024-01-25 23:55:45 -0300

    adicionando components, theme, HomeScreen, Footer, images e robots.txt

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,34 @@
-import Link from "../src/components/Link";
+import HomeScreen from '../src/screens/HomeScreen/index';
+export default HomeScreen;
 
-function Title({ as, children }) {
-    const Tag = as;
-    return(
-        <>
-            <Tag>
-                {children}
-            </Tag>
-            <style jsx>{`
-                ${Tag} {
-                    color: red;
-                }
-            `}</style>
-        </>
-    )
-}
+//  import Link from "../src/components/Link";
+//  import Footer from '../src/components/patterns/Footer/index';
+
+//  function Title({ as, children }) {
+//      const Tag = as;
+//      return(
+//          <>
+//              <Tag>
+//                  {children}
+//              </Tag>
+//              <style jsx>{`
+//                  ${Tag} {
+//                      color: red;
+//                  }
+//              `}</style>
+//          </>
+//      )
+//  }
 
 
-export default function HomePage() {
-    return(
-        <div>
-            <Title as="h1">Alura Cases - Home Page</Title>
-                <Link href="/faq">
-                    Ir para o FAQ
-                </Link>
-        </div>
-    )
-}
+//  export default function HomePage() {
+//      return(
+//          <div>
+//              <Title as="h1">Alura Cases - Home Page</Title>
+//                  <Link href="/faq">
+//                      Ir para o FAQ
+//                  </Link>
+//                  <Footer />
+//          </div>
+//      )
+//  }
